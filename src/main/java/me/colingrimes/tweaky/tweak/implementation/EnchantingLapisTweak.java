cab29e3c38b0dd8e18e475522208739b9a78f8a9
Scill <realscill@gmail.com>
2025-12-14 18:20:43 -0500

    Fix several dupes related to shared enchanting tables

--- src/main/java/me/colingrimes/tweaky/tweak/implementation/EnchantingLapisTweak.java
+++ src/main/java/me/colingrimes/tweaky/tweak/implementation/EnchantingLapisTweak.java
@@ -2,21 +2,31 @@ package me.colingrimes.tweaky.tweak.implementation;
 
 import me.colingrimes.tweaky.Tweaky;
 import me.colingrimes.tweaky.tweak.Tweak;
+import me.colingrimes.tweaky.util.bukkit.NBT;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.block.EnchantingTable;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.EnchantingInventory;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.persistence.PersistentDataType;
 
 import javax.annotation.Nonnull;
+import java.util.*;
 
 public class EnchantingLapisTweak extends Tweak {
+
+	private static final String ENCHANTING_LAPIS_KEY = "enchanting_lapis";
+	private static final Map<Location, SharedEnchantingTable> shared = new HashMap<>();
 
 	public EnchantingLapisTweak(@Nonnull Tweaky plugin) {
 		super(plugin, "enchanting_lapis");
@@ -27,27 +37,13 @@ public class EnchantingLapisTweak extends Tweak {
 		return settings.TWEAK_ENCHANTING_LAPIS.get();
 	}
 
-	@EventHandler
-	public void onInventoryClose(@Nonnull InventoryCloseEvent event) {
-		if (!(event.getInventory() instanceof EnchantingInventory inv) || inv.getLocation() == null) {
-			return;
-		}
-
-		Block block = inv.getLocation().getBlock();
-		if (!(block.getState() instanceof EnchantingTable ench)) {
-			return;
-		}
-
-		int amount = inv.getSecondary() != null ? inv.getSecondary().getAmount() : 0;
-		inv.setSecondary(null);
-
-		NamespacedKey key = new NamespacedKey(plugin, "enchanting_lapis");
-		if (amount == 0) {
-			ench.getPersistentDataContainer().remove(key);
-		} else {
-			ench.getPersistentDataContainer().set(key, PersistentDataType.INTEGER, amount);
-		}
-		ench.update();
+	@Override
+	public void shutdown() {
+		shared.values().forEach(ench -> new HashSet<>(ench.viewers).forEach(viewer -> {
+			ench.close(viewer);
+			viewer.closeInventory();
+		}));
+		shared.clear();
 	}
 
 	@EventHandler
@@ -57,28 +53,156 @@ public class EnchantingLapisTweak extends Tweak {
 		}
 
 		Block block = inv.getLocation().getBlock();
-		if (!(block.getState() instanceof EnchantingTable ench)) {
-			return;
-		}
-
-		NamespacedKey key = new NamespacedKey(plugin, "enchanting_lapis");
-		Integer lapisCount = ench.getPersistentDataContainer().get(key, PersistentDataType.INTEGER);
-		if (lapisCount != null) {
-			inv.setSecondary(new ItemStack(Material.LAPIS_LAZULI, lapisCount));
+		if (block.getState() instanceof EnchantingTable ench) {
+			Player player = (Player) event.getPlayer();
+			shared.computeIfAbsent(inv.getLocation(), __ -> new SharedEnchantingTable(ench)).open(player, inv);
 		}
 	}
 
 	@EventHandler
-	public void onBlockBreak(@Nonnull BlockBreakEvent event) {
-		Block block = event.getBlock();
-		if (!(block.getState() instanceof EnchantingTable ench)) {
+	public void onInventoryClose(@Nonnull InventoryCloseEvent event) {
+		if (!(event.getInventory() instanceof EnchantingInventory inv) || inv.getLocation() == null) {
 			return;
 		}
 
-		NamespacedKey key = new NamespacedKey(plugin, "enchanting_lapis");
-		Integer lapisCount = ench.getPersistentDataContainer().get(key, PersistentDataType.INTEGER);
-		if (lapisCount != null) {
-			block.getWorld().dropItem(block.getLocation(), new ItemStack(Material.LAPIS_LAZULI, lapisCount));
+		SharedEnchantingTable ench = shared.get(inv.getLocation());
+		if (ench != null) {
+			ench.close((Player) event.getPlayer(), inv);
+		}
+	}
+
+	@EventHandler
+	public void onInventoryClick(@Nonnull InventoryClickEvent event) {
+		save((Player) event.getWhoClicked(), event.getInventory());
+	}
+
+	@EventHandler
+	public void onInventoryDrag(@Nonnull InventoryDragEvent event) {
+		save((Player) event.getWhoClicked(), event.getInventory());
+	}
+
+	@EventHandler
+	public void onBlockBreak(@Nonnull BlockBreakEvent event) {
+		SharedEnchantingTable ench = shared.get(event.getBlock().getLocation());
+		if (ench != null) {
+			ench.destroy();
+		}
+	}
+
+	@EventHandler
+	public void onPlayerQuit(@Nonnull PlayerQuitEvent event) {
+		for (SharedEnchantingTable ench : shared.values()) {
+			if (ench.viewers.contains(event.getPlayer())) {
+				ench.close(event.getPlayer());
+			}
+		}
+	}
+
+	private void save(@Nonnull Player player, @Nonnull Inventory inventory) {
+		Location location = inventory.getLocation();
+		if (shared.containsKey(location) && inventory instanceof EnchantingInventory inv) {
+			shared.get(location).save(player, inv);
+		}
+	}
+
+	private class SharedEnchantingTable {
+		private final Set<Player> viewers = new HashSet<>();
+		private final EnchantingTable ench;
+		private int lapisCount;
+
+		public SharedEnchantingTable(@Nonnull EnchantingTable ench) {
+			this.ench = ench;
+			this.lapisCount = NBT.getTag(ench, ENCHANTING_LAPIS_KEY, Integer.class).orElse(0);
+		}
+
+		/**
+		 * Handles the opening of the enchanting table to ensure its shared.
+		 *
+		 * @param player the player
+		 * @param inventory the current enchanting inventory they have opened
+		 */
+		public void open(@Nonnull Player player, @Nonnull EnchantingInventory inventory) {
+			inventory.setSecondary(lapisCount > 0 ? new ItemStack(Material.LAPIS_LAZULI, lapisCount) : null);
+			viewers.add(player);
+		}
+
+		/**
+		 * Handles the closing of the enchanting table for the player.
+		 * <p>
+		 * Stops the default behavior of giving lapis back to the player on close.
+		 *
+		 * @param player the player
+		 */
+		public void close(@Nonnull Player player) {
+			if (viewers.contains(player) && player.getOpenInventory().getTopInventory() instanceof EnchantingInventory inv) {
+				close(player, inv);
+			}
+			viewers.remove(player);
+		}
+
+		/**
+		 * Handles the closing of the enchanting table for the player.
+		 * <p>
+		 * Stops the default behavior of giving lapis back to the player on close.
+		 *
+		 * @param player the player
+		 * @param inventory the current enchanting inventory they have opened
+		 */
+		public void close(@Nonnull Player player, @Nonnull EnchantingInventory inventory) {
+			inventory.setSecondary(null);
+			viewers.remove(player);
+		}
+
+		/**
+		 * Handles the saving of the enchanting table when a player makes a change.
+		 *
+		 * @param player the player
+		 * @param inventory the current enchanting inventory they have opened
+		 */
+		public void save(@Nonnull Player player, @Nonnull EnchantingInventory inventory) {
+			if (!viewers.contains(player)) {
+				return;
+			}
+
+			// Delays it by 1 tick to make sure the inventory finished updating.
+			Bukkit.getScheduler().runTask(plugin, () -> {
+				lapisCount = inventory.getSecondary() != null ? inventory.getSecondary().getAmount() : 0;
+				if (lapisCount == 0) {
+					NBT.removeTag(ench, ENCHANTING_LAPIS_KEY);
+				} else {
+					NBT.setTag(ench, ENCHANTING_LAPIS_KEY, lapisCount);
+				}
+
+				ench.update();
+
+				// Update all viewers.
+				for (Player viewer : viewers) {
+					if (!viewer.equals(player) && viewer.getOpenInventory().getTopInventory() instanceof EnchantingInventory inv) {
+						open(player, inv);
+					}
+				}
+			});
+		}
+
+		/**
+		 * Handles the destruction of the enchanting table.
+		 * <p>
+		 * Ensures any remaining lapis is dropped from the broken enchanting table.
+		 */
+		public void destroy() {
+			for (Player viewer : viewers) {
+				close(viewer);
+			}
+
+			Location location = ench.getLocation();
+			if (location.getWorld() == null) {
+				return;
+			}
+
+			shared.remove(location);
+			if (lapisCount > 0) {
+				location.getWorld().dropItem(location, new ItemStack(Material.LAPIS_LAZULI, lapisCount));
+			}
 		}
 	}
 }
