f68b7b8846cfb112f0697caaec150b67ac777630
Scill <realscill@gmail.com>
2026-03-14 13:50:05 -0400

    Add shift-click for XP conversions

--- src/main/java/me/colingrimes/tweaky/tweak/implementation/convenience/XpFillTweak.java
+++ src/main/java/me/colingrimes/tweaky/tweak/implementation/convenience/XpFillTweak.java
@@ -2,14 +2,17 @@ package me.colingrimes.tweaky.tweak.implementation.convenience;
 
 import me.colingrimes.tweaky.Tweaky;
 import me.colingrimes.tweaky.event.PlayerInteractBlockEvent;
+import me.colingrimes.tweaky.scheduler.Scheduler;
 import me.colingrimes.tweaky.tweak.event.TweakHandler;
 import me.colingrimes.tweaky.tweak.type.DefaultTweak;
 import me.colingrimes.tweaky.tweak.properties.TweakProperties;
 import me.colingrimes.tweaky.util.bukkit.Experience;
 import me.colingrimes.tweaky.util.bukkit.Items;
 import me.colingrimes.tweaky.util.bukkit.Players;
+import me.colingrimes.tweaky.util.bukkit.Sounds;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
@@ -31,9 +34,24 @@ public class XpFillTweak extends DefaultTweak {
 
 	@TweakHandler(ignoreCancelled = true)
 	public void onPlayerInteract(@Nonnull PlayerInteractBlockEvent event) {
-		Players.use(event.getPlayer(), event.getHand(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, event.getLocation());
-		Items.give(event.getPlayer(), new ItemStack(Material.EXPERIENCE_BOTTLE));
-		Experience.remove(event.getPlayer(), settings.TWEAK_XP_FILL_COST.get());
+		Player player = event.getPlayer();
+		if (!player.isSneaking()) {
+			Players.use(event.getPlayer(), event.getHand(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, event.getLocation());
+			Items.give(event.getPlayer(), new ItemStack(Material.EXPERIENCE_BOTTLE));
+			Experience.remove(event.getPlayer(), settings.TWEAK_XP_FILL_COST.get());
+			event.setCancelled(true);
+			return;
+		}
+
+		int bottles = player.getInventory().getItem(event.getHand()).getAmount();
+		double maxConversions = (double) Experience.fromPlayer(player) / settings.TWEAK_XP_FILL_COST.get();
+		int conversions = Math.min(bottles, (int) maxConversions);
+
+		player.swingHand(event.getHand());
+		player.getInventory().getItem(event.getHand()).setAmount(bottles - conversions);
+		Scheduler.sync().run(() -> Items.give(event.getPlayer(), new ItemStack(Material.EXPERIENCE_BOTTLE, conversions)));
+		Experience.remove(event.getPlayer(), settings.TWEAK_XP_FILL_COST.get() * conversions);
+		Sounds.play(event.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP);
 		event.setCancelled(true);
 	}
 }
