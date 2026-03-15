fb1d99343e2b93c18fbc040b71bc81d91eeff75d
Scill <realscill@gmail.com>
2026-03-14 23:57:03 -0400

    Add new vehicle mob tweak

---
+++ src/main/java/me/colingrimes/tweaky/tweak/implementation/mob/VehicleMobTweak.java
@@ -0,0 +1,91 @@
+package me.colingrimes.tweaky.tweak.implementation.mob;
+
+import me.colingrimes.tweaky.Tweaky;
+import me.colingrimes.tweaky.tweak.event.TweakHandler;
+import me.colingrimes.tweaky.tweak.properties.TweakProperties;
+import me.colingrimes.tweaky.tweak.type.DefaultTweak;
+import me.colingrimes.tweaky.util.bukkit.Items;
+import org.bukkit.entity.*;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Objects;
+
+public class VehicleMobTweak extends DefaultTweak {
+
+	public VehicleMobTweak(@Nonnull Tweaky plugin) {
+		super(plugin, "vehicle_mobs");
+	}
+
+	@Override
+	protected void configureProperties(@Nonnull TweakProperties properties) {
+		properties.getGuard()
+				.item(i -> getVehicleEntity(i) != null)
+				.entity(this::canRideVehicle);
+	}
+
+	@TweakHandler
+	public void onPlayerInteract(@Nonnull PlayerInteractEntityEvent event) {
+		Player player = event.getPlayer();
+		Entity entity = event.getRightClicked();
+		ItemStack item = player.getInventory().getItem(event.getHand());
+		EntityType type = Objects.requireNonNull(getVehicleEntity(item));
+
+		Vehicle vehicle = (Vehicle) entity.getWorld().spawnEntity(entity.getLocation(), type);
+		vehicle.addPassenger(entity);
+
+		Items.remove(item);
+		player.swingHand(event.getHand());
+	}
+
+	/**
+	 * Converts a vehicle item into its corresponding entity form.
+	 *
+	 * @param item the item to convert
+	 * @return the vehicle type
+	 */
+	@Nullable
+	private EntityType getVehicleEntity(@Nonnull ItemStack item) {
+		return switch (item.getType()) {
+			// Boats
+			case OAK_BOAT               -> EntityType.OAK_BOAT;
+			case SPRUCE_BOAT            -> EntityType.SPRUCE_BOAT;
+			case BIRCH_BOAT             -> EntityType.BIRCH_BOAT;
+			case JUNGLE_BOAT            -> EntityType.JUNGLE_BOAT;
+			case ACACIA_BOAT            -> EntityType.ACACIA_BOAT;
+			case DARK_OAK_BOAT          -> EntityType.DARK_OAK_BOAT;
+			case MANGROVE_BOAT          -> EntityType.MANGROVE_BOAT;
+			case CHERRY_BOAT            -> EntityType.CHERRY_BOAT;
+			case PALE_OAK_BOAT          -> EntityType.PALE_OAK_BOAT;
+			case BAMBOO_RAFT            -> EntityType.BAMBOO_RAFT;
+			// Mine Carts
+			case MINECART               -> EntityType.MINECART;
+			case CHEST_MINECART         -> EntityType.CHEST_MINECART;
+			case FURNACE_MINECART       -> EntityType.FURNACE_MINECART;
+			case TNT_MINECART           -> EntityType.TNT_MINECART;
+			case HOPPER_MINECART        -> EntityType.HOPPER_MINECART;
+			case COMMAND_BLOCK_MINECART -> EntityType.COMMAND_BLOCK_MINECART;
+			// Default
+			default -> null;
+		};
+	}
+
+	/**
+	 * Checks if the entity can go inside a vehicle.
+	 *
+	 * @param entity the entity
+	 * @return true if the entity can go inside a vehicle
+	 */
+	private boolean canRideVehicle(@Nonnull Entity entity) {
+		if (!(entity instanceof LivingEntity)) {
+			return false;
+		}
+
+		return switch (entity.getType()) {
+			case ENDER_DRAGON, WITHER, WARDEN, ELDER_GUARDIAN, RAVAGER -> false;
+			default -> true;
+		};
+	}
+}
